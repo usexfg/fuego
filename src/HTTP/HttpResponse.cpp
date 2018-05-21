13796664df41aca3a0a1aa6089fa534e7f531d5d
æjontargaryen <34797913+aejontargaryen@users.noreply.github.com>
2018-05-21 18:06:48 -0400

    Update HttpResponse.cpp

--- src/HTTP/HttpResponse.cpp
+++ src/HTTP/HttpResponse.cpp
@@ -43,6 +43,7 @@ namespace CryptoNote {
 HttpResponse::HttpResponse() {
   status = STATUS_200;
   headers["Server"] = "{{{{DRGL}}}} HTTP server";
+  headers["Access-Control-Allow-Origin"] = "*";
 }
 
 void HttpResponse::setStatus(HTTP_STATUS s) {

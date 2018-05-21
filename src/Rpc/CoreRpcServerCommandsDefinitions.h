88d0c3307e9fa9de88e8642a04015958e46f3b3d
æjontargaryen <34797913+aejontargaryen@users.noreply.github.com>
2018-05-21 17:27:50 -0400

    Update CoreRpcServerCommandsDefinitions.h

--- src/Rpc/CoreRpcServerCommandsDefinitions.h
+++ src/Rpc/CoreRpcServerCommandsDefinitions.h
@@ -1,4 +1,6 @@
+// Copyright (c) 2018 {DRGL}
 // Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) Karbowanec
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 

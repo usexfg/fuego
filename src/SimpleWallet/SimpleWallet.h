b7230569cef028c572762a6d0a0698c72fb17ea6
aejontargaryen <aejontargaryen@protonmail.com>
2018-11-16 20:35:51 -0500

    checkpoint load

--- src/SimpleWallet/SimpleWallet.h
+++ src/SimpleWallet/SimpleWallet.h
@@ -133,7 +133,6 @@ namespace CryptoNote
 	  
 #ifndef __ANDROID__
 	std::string resolveAlias(const std::string& aliasUrl);
-	bool fetch_dns_txt(const std::string domain, std::string &record);
 #endif
 
     bool ask_wallet_create_if_needed();

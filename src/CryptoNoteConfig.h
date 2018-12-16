0d1f257cdb06ccfa5d69bfe41e2c8948541940c3
ÆJT <34797913+aejontargaryen@users.noreply.github.com>
2018-12-16 00:45:35 -0500

    upgrade height

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -90,7 +90,7 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 const uint32_t UPGRADE_HEIGHT_V2                             = 147958; //{Hardhome}
 const uint32_t UPGRADE_HEIGHT_V3                             = 154321; //{Longclaw}
 const uint32_t UPGRADE_HEIGHT_V4                             = 300000; //{Dracarys}
-const uint32_t UPGRADE_HEIGHT_V5                             = 324810; //{Ironborn} CNv7
+const uint32_t UPGRADE_HEIGHT_V5                             = 324819; //{Ironborn} CNv7
   
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90; // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks

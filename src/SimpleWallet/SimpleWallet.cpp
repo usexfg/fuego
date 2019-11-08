7cc487c41f5db6bbab6fc00cea8df38ee113c264
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2019-11-08 18:42:48 -0500

    Change OpenAlias prefix
    oa1:xfg

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -543,8 +543,8 @@ bool processServerAliasResponse(const std::string& s, std::string& address) {
 	try {
 
 		// Courtesy of Monero Project
-		// make sure the txt record has "oa1:DRGL" and find it
-		auto pos = s.find("oa1:DRGL");
+		// make sure the txt record has "oa1:xfg" and find it
+		auto pos = s.find("oa1:xfg");
 		if (pos == std::string::npos)
 			return false;
 		// search from there to find "recipient_address="

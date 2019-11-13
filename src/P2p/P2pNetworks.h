595185e21640d6b627f8e7e845219367f0e2aa22
ÆJT <34797913+aejontargaryen@users.noreply.github.com>
2019-11-12 20:55:00 -0500

    Change Network ID

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -1,35 +1,26 @@
-
-// {DRGL} Kills White Walkers
-
-// 2018 {DRÆGONGLASS}
-// <http://www.ZirtysPerzys.org>
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers, The Karbowanec developers
-// This file is part of Bytecoin.
-// Bytecoin is free software: you can redistribute it and/or modify
+// Copyright (c) 2018-2019, Fandom GOLD Project
+//
+// This file is part of Fandom GOLD.
+// Fandom GOLD is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// Bytecoin is distributed in the hope that it will be useful,
+// Fandom GOLD is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // You should have received a copy of the GNU Lesser General Public License
-// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+// along with Fandom GOLD.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
-#include "../CryptoNoteConfig.h"
 
 #pragma once
 
 namespace CryptoNote
 {
-	namespace
-	{
-		boost::uuids::uuid name;
-		boost::uuids::name_generator gen(name);
-		boost::uuids::uuid u = gen(GENESIS_COINBASE_TX_HEX);
-	}
-	const static boost::uuids::uuid BYTECOIN_NETWORK = u;
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x46, 0x41, 0x4e, 0x44, 0x4f, 0x4d, 0x20, 0x47, 0x4f, 0x4c, 0x44, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10  } };
+
 }

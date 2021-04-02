d668795eaa4286a530d2686e05fa3737b4c8db79
ÆJT <aejontargaryen@protonmail.com>
2021-04-01 23:21:05 -0400

    Save daemon command

--- src/Daemon/DaemonCommandsHandler.h
+++ src/Daemon/DaemonCommandsHandler.h
@@ -2,21 +2,22 @@
 // Copyright (c) 2014-2018, The Monero project
 // Copyright (c) 2014-2018, The Forknote developers
 // Copyright (c) 2016-2018, The Karbowanec developers
+// Copyright (c) 2017-2021, Fandom Gold Society
 //
-// This file is part of Bytecoin.
+// This file is part of Fango.
 //
-// Bytecoin is free software: you can redistribute it and/or modify
+// Fango is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Bytecoin is distributed in the hope that it will be useful,
+// Fango is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+// along with Fango.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
@@ -84,5 +85,6 @@ private:
   bool print_ban(const std::vector<std::string>& args);
   bool ban(const std::vector<std::string>& args);
   bool unban(const std::vector<std::string>& args);
+  bool save(const std::vector<std::string>& args);
   bool status(const std::vector<std::string>& args);
 };

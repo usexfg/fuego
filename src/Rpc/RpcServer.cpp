79f33f1afd7d9cbf578a1e8dd34c1320e3a009d8
aejontargaryen <aejontargaryen@protonmail.com>
2021-05-29 01:22:26 +0000

    block major version for /getinfo

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -382,6 +382,7 @@ bool RpcServer::on_get_info(const COMMAND_RPC_GET_INFO::request& req, COMMAND_RP
     // that large uint64_t number is unsafe in JavaScript environment and therefore as a JSON value so we display it as a formatted string
   res.already_generated_coins = m_core.currency().formatAmount(m_core.getTotalGeneratedAmount());
   res.status = CORE_RPC_STATUS_OK;
+  res.block_major_version = m_core.getCurrentBlockMajorVersion();
   res.start_time = m_core.getStartTime();
   return true;
 }

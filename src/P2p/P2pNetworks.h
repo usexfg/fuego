105cbbcc8f0d8bb30799bc6dc73869c08e64cc9f
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2021-09-13 07:40:56 +0000

    TESTNET ID

--- src/P2p/P2pNetworks.h
+++ src/P2p/P2pNetworks.h
@@ -1,22 +1,22 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers, The Karbowanec developers
-// Copyright (c) 2018-2019, The Fandom GOLD Project
+// Copyright (c) 2018-2021, Fandom Gold Society
 //
-// This file is part of Fandom GOLD.
-// Fandom GOLD is free software: you can redistribute it and/or modify
+// This file is part of Fango.
+// Fango is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// Fandom GOLD is distributed in the hope that it will be useful,
+// Fango is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // You should have received a copy of the GNU Lesser General Public License
-// along with Fandom GOLD.  If not, see <http://www.gnu.org/licenses/>.
+// along with Fango.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
 namespace CryptoNote
 {
-  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x46, 0x41, 0x4e, 0x44, 0x4f, 0x4d, 0x47, 0x4f, 0x4c, 0x44, 0x00, 0x12, 0x10, 0x11, 0x01, 0x10  } };
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x46, 0x41, 0x4e, 0x44, 0x4f, 0x4d, 0x47, 0x4f, 0x4c, 0x44, 0x3c, 0x54, 0x45, 0x53, 0x54, 0x3e  } };
 
 }

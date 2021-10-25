72954356eec347c9c169bab1ba67568c359b6658
ÆJT <aejontargaryen@protonmail.com>
2021-10-25 11:27:58 -0400

    core: check_fee (#48)
    * use minimum fee
    
    * add command to generate paymentID
    
    * generate payment ID command
    
    * core check_tx_fee
    
    Co-authored-by: ColinRitman <54386876+ColinRitman@users.noreply.github.com>

--- src/CryptoNoteCore/Core.h
+++ src/CryptoNoteCore/Core.h
@@ -18,6 +18,7 @@
 
 #pragma once
 
+#include <ctime>
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/variables_map.hpp>
 
@@ -149,6 +150,7 @@ namespace CryptoNote {
     void print_blockchain(uint32_t start_index, uint32_t end_index);
     void print_blockchain_index();
     std::string print_pool(bool short_format);
+    std::list<CryptoNote::tx_memory_pool::TransactionDetails> getMemoryPool() const;
     void print_blockchain_outs(const std::string &file);
     virtual bool getPoolChanges(const Crypto::Hash &tailBlockId, const std::vector<Crypto::Hash> &knownTxsIds,
                                 std::vector<Transaction> &addedTxs, std::vector<Crypto::Hash> &deletedTxsIds) override;
@@ -161,7 +163,7 @@ namespace CryptoNote {
     uint64_t getTotalGeneratedAmount();
     uint64_t fullDepositAmount() const;
     uint64_t depositAmountAtHeight(size_t height) const;
-     uint8_t getBlockMajorVersionForHeight(uint32_t height) const;
+    uint8_t getBlockMajorVersionForHeight(uint32_t height) const;
 
     bool is_key_image_spent(const Crypto::KeyImage &key_im);
 
@@ -171,11 +173,10 @@ namespace CryptoNote {
     bool parse_tx_from_blob(Transaction &tx, Crypto::Hash &tx_hash, Crypto::Hash &tx_prefix_hash, const BinaryArray &blob);
     bool handle_incoming_block(const Block &b, block_verification_context &bvc, bool control_miner, bool relay_block);
 
-    bool check_tx_syntax(const Transaction &tx);
-    //check correct values, amounts and all lightweight checks not related with database
-    bool check_tx_semantic(const Transaction &tx, bool keeped_by_block, uint32_t &height);
-    //check if tx already in memory pool or in main blockchain
-     bool check_tx_mixin(const Transaction& tx);
+    bool check_tx_syntax(const Transaction &tx);  //check correct values, amounts and all lightweight checks not related with database
+    bool check_tx_semantic(const Transaction &tx, bool keeped_by_block, uint32_t &height); //check if tx already in memory pool or in main blockchain
+    bool check_tx_mixin(const Transaction& tx);   //check if the mixin is not too large
+    bool check_tx_fee(const Transaction& tx, size_t blobSize, tx_verification_context& tvc); //check for proper tx fee
 
     bool check_tx_ring_signature(const KeyInput &tx, const Crypto::Hash &tx_prefix_hash, const std::vector<Crypto::Signature> &sig);
     bool is_tx_spendtime_unlocked(uint64_t unlock_time);

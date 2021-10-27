f8a87cb3a71529ffee37623fd1b3b2657145f1e1
ColinRitman <54386876+ColinRitman@users.noreply.github.com>
2021-10-27 01:27:23 -0400

    Correct minimum fee

--- src/PaymentGate/PaymentServiceJsonRpcMessages.h
+++ src/PaymentGate/PaymentServiceJsonRpcMessages.h
@@ -511,7 +511,7 @@ struct SendTransaction
     std::vector<std::string> sourceAddresses;
     std::vector<WalletRpcOrder> transfers;
     std::string changeAddress;
-    uint64_t fee = 1000;
+    uint64_t fee = CryptoNote::parameters::MINIMUM_FEE;
     uint32_t anonymity = DEFAULT_ANONYMITY_LEVEL;
     std::string extra;
     std::string paymentId;
@@ -536,7 +536,7 @@ struct CreateDelayedTransaction
     std::vector<std::string> addresses;
     std::vector<WalletRpcOrder> transfers;
     std::string changeAddress;
-    uint64_t fee = 1000;
+    uint64_t fee = CryptoNote::parameters::MINIMUM_FEE;
     uint32_t anonymity = DEFAULT_ANONYMITY_LEVEL;
     std::string extra;
     std::string paymentId;
